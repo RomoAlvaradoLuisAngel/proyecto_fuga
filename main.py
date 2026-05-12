8275b88acbaa62a4d5d2af15e6f30e8929dfce08
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-05-12 11:04:29 -0600

    modificaciones

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Proyecto Fuga"
-    page.add(ft.Text("¡Bienvenido al Proyecto Fuga!"))
+def main(page : ft.Page):
+    page.title="BlueLeak"
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.padding=30
+    page.bgcolor = "#00B0C8"
+    
+    page.add(ft.Text("¡Bienvenido a blueLeak!"))
+    
+    def entrar_click(e): 
+        page.add(ft.Text("Bienvenido"))
+    
+    btn_entrar = ft.ElevatedButton(
+        content="Entrar a la aplicación",
+        bgcolor=ft.Colors.GREEN_400,
+        color=ft.Colors.WHITE,
+        on_click=entrar_click
+    )
+
+    page.add(btn_entrar)
+    
+    def mostrar_pantalla_principal():
+        page.clean()
+        page.navigation_bar=ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
+                ft.NavigationBarDestination(icon=ft.Icons.INFO, label="Informacion"),
+                ft.NavigationBarDestination(icon=ft.Icons.PERSON, label="Inicio de sesion"),
+            ],
+            on_change = lambda e: print(f"Seleccionado: {e.control.selected_index}")
+        )
+
     
 ft.app(target=main)
