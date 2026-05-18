d32154a999ef08a54140e5d9c6c12c2ede7c69fd
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-05-18 09:03:33 -0600

    cambio de pantalla

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flet as ft
+from flet_geolocator import Geolocator
+import webbrowser
 
 def main(page : ft.Page):
     page.title="BlueLeak"
@@ -9,20 +11,9 @@ def main(page : ft.Page):
     
     page.add(ft.Text("¡Bienvenido a blueLeak!"))
     
-    def entrar_click(e): 
-        page.add(ft.Text("Bienvenido"))
-    
-    btn_entrar = ft.ElevatedButton(
-        content="Entrar a la aplicación",
-        bgcolor=ft.Colors.GREEN_400,
-        color=ft.Colors.WHITE,
-        on_click=entrar_click
-    )
-
-    page.add(btn_entrar)
-    
     def mostrar_pantalla_principal():
         page.clean()
+        page.add(ft.Text("Hola de prueba"))
         page.navigation_bar=ft.NavigationBar(
             destinations=[
                 ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
@@ -31,6 +22,59 @@ def main(page : ft.Page):
             ],
             on_change = lambda e: print(f"Seleccionado: {e.control.selected_index}")
         )
+        
+        page.add(
+            ft.AppBar(
+                title=ft.Text("Panel principal"),
+                bgcolor=ft.Colors.BLUE_900,
+                color=ft.Colors.WHITE,
+                automatically_imply_leading=False
+            ),
+            ft.Column(
+                [
+                    ft.Icon(ft.Icons.BOLT, color=ft.Colors.YELLOW, size=100),
+                    ft.Text("Bienvenido al sistema.", color=ft.Colors.WHITE),
+                    ft.Text("Has iniciado sesion correctamente.", color=ft.Colors.WHITE)
+                ],
+                expand=True,
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )
+        )
+        page.update()
+    
+    btn_entrar = ft.ElevatedButton(
+        content="Entrar a la aplicación",
+        bgcolor=ft.Colors.GREEN_400,
+        color=ft.Colors.WHITE,
+        on_click=mostrar_pantalla_principal
+    )
+
+    page.add(btn_entrar)
+    
+        
+    geo = Geolocator()
+    page.services.append(geo)
+
+    async def abrir_mapa(e):
+
+        pos = await geo.get_current_position()
+
+        if not pos:
+            page.add(ft.Text("No se pudo obtener ubicación"))
+            return
+
+        url = (
+            f"https://www.openstreetmap.org/"
+            f"?mlat={pos.latitude}&mlon={pos.longitude}"
+            f"#map=16/{pos.latitude}/{pos.longitude}"
+        )
+
+        webbrowser.open(url)
+
+    page.add(
+        ft.Button("Abrir mapa", on_click=abrir_mapa)
+    )
 
     
-ft.app(target=main)
+ft.run(main)
