582dacecabda50e475cbc61e1f4c21954a9f7436
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-05-18 09:13:33 -0600

    lol

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import flet as ft
 from flet_geolocator import Geolocator
 import webbrowser
+import mysql.connector
+
+conexion = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="ges_fugas"
+)
 
 def main(page : ft.Page):
     page.title="BlueLeak"
