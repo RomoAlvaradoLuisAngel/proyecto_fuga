2b7015197a753707dd6496e21326349357dc618f
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-05-21 09:12:47 -0600

    arreglar navigation bar

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ async def main(page : ft.Page):
     geo = ftg.Geolocator()
     page.services.append(geo)
     
+    vista_contenedor = ft.Container(expand=True)
+    
     latitud = None
     longitud = None
     
@@ -40,20 +42,58 @@ async def main(page : ft.Page):
         except Exception as ex:
             page.add(ft.Text(f"Error al obtener ubicación: {ex}"))
             page.update()
-    
+            
+    def inicio():
+        return ft.Column([
+            ft.Icon(ft.Icons.WATER_DROP, size=60, color=ft.Colors.BLUE_900),
+            ft.Text("¡Bienvenido a BlueLeak!", size=24, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_900),
+            ft.Button("Abrir mapa.", on_click=abrir_mapa, bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
+        ])
+
+        
+    def informacion():
+        return ft.Column([
+            ft.Icon(ft.Icons.INFO, size=60, color=ft.Colors.BLUE_900),
+            ft.Text("Información de la aplicación", size=24, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_900),
+            ft.Text("BlueLeak es una aplicación diseñada para ayudar a los usuarios a reportar fugas de agua en su área. Con esta aplicación, los usuarios pueden compartir información sobre la ubicación de las fugas, lo que permite a las autoridades locales tomar medidas rápidas para solucionar el problema y reducir el desperdicio de agua.", size=16, color=ft.Colors.BLUE_900)
+        ])
+
+        
+    def inicio_sesion():
+        return ft.Column(
+            [
+            ft.Text("Inicio de sesión", size=24, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_900),
+            ft.TextField(label="Usuario", width=200),
+            ft.TextField(label="Contraseña", width=200, password=True),
+            ft.Button("Iniciar sesión", bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
+        ])
+
+
+    def cambiar_pantalla(e):
+        opcion = e.control.selected_index
+        
+        if opcion == 0:
+            vista_contenedor.content=inicio()
+        elif opcion == 1:
+            vista_contenedor.content=informacion()
+        elif opcion == 2:
+            vista_contenedor.content=inicio_sesion()
+        page.update()
+
     def mostrar_pantalla_principal(e):
         page.clean()
         
         page.navigation_bar=ft.NavigationBar(
+            selected_index=0,
                 destinations=[
                     ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
                     ft.NavigationBarDestination(icon=ft.Icons.INFO, label="Informacion"),
                     ft.NavigationBarDestination(icon=ft.Icons.PERSON, label="Inicio de sesion"),
                 ],
-                on_change = lambda e: print(f"Seleccionado: {e.control.selected_index}")
+                on_change = cambiar_pantalla
             )
         page.appbar = ft.AppBar(
-                title=ft.Text("Panel principal"),
+                title=ft.Text("Bienvenido a BlueLeak"),
                 bgcolor=ft.Colors.BLUE_900,
                 color=ft.Colors.WHITE
                 )
@@ -62,6 +102,11 @@ async def main(page : ft.Page):
                 ft.Button("Abrir mapa.", on_click=abrir_mapa, bgcolor=ft.Colors.BLUE_400, color=ft.Colors.WHITE)
             )
         page.update()
+        
+    vista_contenedor.content=inicio()
+        
+    page.add(vista_contenedor)
+    page.update()
     
     page.add(
         ft.Icon(ft.Icons.WATER_DROP, size=60, color=ft.Colors.WHITE),
